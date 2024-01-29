fbc3950b095134a1d8682c634054fff0d017c831
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-01-29 18:04:08 +0100

    changed migration for db compatibility

--- src/Migration/ContentSubcolumnMigration.php
+++ src/Migration/ContentSubcolumnMigration.php
@@ -57,23 +57,76 @@ class ContentSubcolumnMigration implements MigrationInterface
         try
         {
             $qb = $this->connection->createQueryBuilder()
-                ->select('id, type, sc_type')
+                ->select('count(id) AS count')
                 ->from('tl_content')
-                ->where('type IN (:types)')
-                ->andWhere('sc_columnset = "" OR sc_columnset IS NULL')
-                ->andWhere('sc_parent > "0" AND sc_parent != "" AND sc_parent IS NOT NULL')
-                ->andWhere('sc_type IS NOT NULL AND sc_type != "" AND sc_type != "deprecated"')
-                ->setParameter('types', ['colsetStart', 'colsetPart', 'colsetEnd'], Connection::PARAM_STR_ARRAY);
+                ->where('columnset_id != "" AND columnset_id IS NOT NULL');
 
-            $iterator = $qb->execute()->iterateAssociative();
+            $count = (int)($qb->execute()->fetchOne() ?: 0);
 
-            foreach ($iterator as $item) {
-                $scType = $item['sc_type'] ?? '';
-                $scColumnset = "globals.bootstrap3.$scType";
-                $this->connection->update('tl_content', ['sc_columnset' => $scColumnset], ['id' => $item['id']]);
+            if ($count > 0)
+            {
+                /* ============================================================================================================ */
+                /* Migrate from database sources to new identifier                                                              */
+                /* ============================================================================================================ */
+
+                $qb = $this->connection->createQueryBuilder()
+                    ->select('id, type, sc_parent, columnset_id')
+                    ->from('tl_content')
+                    ->where('type IN (:types)')
+                    ->andWhere('sc_columnset = "" OR sc_columnset IS NULL')
+                    ->andWhere('sc_parent > "0" AND sc_parent != "" AND sc_parent IS NOT NULL')
+                    ->andWhere('sc_type IS NOT NULL AND sc_type != "" AND sc_type != "deprecated"')
+                    ->orderBy('type', 'DESC')
+                    ->addOrderBy('columnset_id', 'DESC')
+                    ->addOrderBy('sc_parent', 'ASC')
+                    ->setParameter('types', ['colsetStart', 'colsetPart', 'colsetEnd'], Connection::PARAM_STR_ARRAY);
+
+                $iterator = $qb->execute()->iterateAssociative();
+
+                $scParentsColumnsetMap = [];
+
+                foreach ($iterator as $item)
+                {
+                    if ($item['type'] === 'colsetStart' && $item['columnset_id']) {
+                        $scParentsColumnsetMap[$item['id']] = $item['columnset_id'];
+                    }
+
+                    $columnsetId = $scParentsColumnsetMap[$item['sc_parent']] ?? null;
+                    if (!$columnsetId) {
+                        continue;
+                    }
+
+                    $scColumnset = "db.tl_columnset.$columnsetId";
+                    $this->connection->update('tl_content', ['sc_columnset' => $scColumnset], ['id' => $item['id']]);
+                }
+            }
+            else
+            {
+                /* ============================================================================================================ */
+                /* Migrate from old config sources to new identifier                                                            */
+                /* ============================================================================================================ */
+
+                $qb = $this->connection->createQueryBuilder()
+                    ->select('id, type, sc_type')
+                    ->from('tl_content')
+                    ->where('type IN (:types)')
+                    ->andWhere('sc_columnset = "" OR sc_columnset IS NULL')
+                    ->andWhere('sc_parent > "0" AND sc_parent != "" AND sc_parent IS NOT NULL')
+                    ->andWhere('sc_type IS NOT NULL AND sc_type != "" AND sc_type != "deprecated"')
+                    ->setParameter('types', ['colsetStart', 'colsetPart', 'colsetEnd'], Connection::PARAM_STR_ARRAY);
+
+                $iterator = $qb->execute()->iterateAssociative();
+
+                foreach ($iterator as $item) {
+                    $scType = $item['sc_type'] ?? null;
+                    if ($scType === null) {
+                        continue;
+                    }
+                    $scColumnset = "globals.bootstrap3.$scType";
+                    $this->connection->update('tl_content', ['sc_columnset' => $scColumnset], ['id' => $item['id']]);
+                }
             }
 
-            $kernel = System::getContainer()->get('kernel');
             $fileName = $this->projectDir . '/vendor/heimrichhannot/contao-subcolumns-bootstrap-bundle/.migrated';
 
             file_put_contents($fileName, '');
